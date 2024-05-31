f5e1a4b133a7c0c75b9dc61470da29d041070bf8
PerpetuumG <perpetuumg@gmail.com>
2024-05-31 13:59:36 +0300

    create project

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+// import Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
+// import glightbox
+import 'glightbox/dist/css/glightbox.css';
+
+import React from 'react';
+import type { Metadata } from 'next';
+import { Playfair_Display } from 'next/font/google';
+import './globals.css';
+
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-playfair-display',
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: 'Restaurant',
+  description: 'Restaurant App',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={playfair.className}>{children}</body>
+      <script
+        // src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+        src={'../bootstrap/bootstrap'}
+        crossOrigin='anonymous'
+      ></script>
+    </html>
+  );
+}
