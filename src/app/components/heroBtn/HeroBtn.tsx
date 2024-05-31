afab0b16423e8d2b5406f09a03f08ae7e314e9f4
PerpetuumG <perpetuumg@gmail.com>
2024-05-31 19:12:47 +0300

    HeroBtn

---
+++ src/app/components/heroBtn/HeroBtn.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './heroBtn.css';
+
+const HeroBtn = ({ name, target }: { name: string; target: string }) => {
+  const handleScrollTo = (section: string) => {};
+
+  return (
+    <a
+      onClick={() => handleScrollTo(target)}
+      className={`btn-hero animated fadeInUp scrollto ${name.includes('book') ? 'ms-4' : undefined}`}
+    >
+      {name}
+    </a>
+  );
+};
+
+export default HeroBtn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Header from '@/app/components/header/Header';
+import Hero from '@/app/sections/hero/Hero';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Hero/>
     </>
   );
 }
