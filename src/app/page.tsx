980309e2e330f8b881b7230fa612efe96a00eb91
PerpetuumG <perpetuumg@gmail.com>
2024-06-06 08:58:00 +0300

    MenuItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/app/sections/hero/Hero';
 import About from '@/app/sections/about/About';
 import WhyUs from '@/app/sections/whyUs/WhyUs';
+import Menu from '@/app/sections/menu/Menu';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main id={'main'}>
         <About />
         <WhyUs />
+        <Menu/>
       </main>
     </>
   );
